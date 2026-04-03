9b8f5ed2179f8000bc8f8b93e7d88b2cc27c534a
darkCapricornus <harishdeepabalavenkatraman@gmail.com>
2026-04-03 18:34:26 +0530

    merged with V1

--- manufacturing_unit/data_gateway/core/interfaces.py
+++ manufacturing_unit/data_gateway/core/interfaces.py
@@ -6,7 +6,11 @@ class ISource(ABC):
     Interface for data sources (e.g. REST, OPC UA, PLC).
     """
     @abstractmethod
+<<<<<<< HEAD
+    def read(self) -> Dict[str, Any]:
+=======
     async def read(self) -> Dict[str, Any]:
+>>>>>>> v1
         """
         Reads data from the source.
         Returns a dictionary of {tag_name: value}.
@@ -18,7 +22,11 @@ class ISink(ABC):
     Interface for data sinks (e.g. Rapid SCADA File, API, MQTT).
     """
     @abstractmethod
+<<<<<<< HEAD
+    def write(self, data: Dict[int, Any]) -> None:
+=======
     async def write(self, data: Dict[int, Any]) -> None:
+>>>>>>> v1
         """
         Writes data to the sink.
         Expecting a dictionary of {channel_id: value}.
@@ -27,9 +35,17 @@ class ISink(ABC):
 
 class IAdapter(ABC):
     @abstractmethod
+<<<<<<< HEAD
+    def connect(self):
+        pass
+        
+    @abstractmethod
+    def disconnect(self):
+=======
     async def connect(self):
         pass
         
     @abstractmethod
     async def disconnect(self):
+>>>>>>> v1
         pass
